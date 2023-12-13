5e36d1a78e254059c8503fd38c2516beccad1df4
GRAviTY-GAGAN <gaganbn1999@gmail.com>
2023-12-13 21:37:36 +0530

    chore: renamed files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ApolloWrapper } from '@/graphql/apolloClient';
 import { ChakraProvider } from '@chakra-ui/react';
-import ReduxProvider from '@/redux/ReduxProvider';
+import { ApolloWrapper } from '@/graphQl/apolloClient';
+import ReduxProvider from '@/redux/reduxProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
