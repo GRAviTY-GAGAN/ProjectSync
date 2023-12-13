b7cd94af9c25c9c14bad30d1c52429f497fbaa18
GRAviTY-GAGAN <gaganbn1999@gmail.com>
2023-12-13 07:41:57 +0530

    first commit

---
+++ GraphQL/ApolloClient.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRApolloClient,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+const apolloClient = () => {
+  const httpLink = new HttpLink({
+    uri: "",
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+};
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={apolloClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
+
+export default apolloClient;
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Box, Button } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      page
+      <Box>
+        <Button>Test Button</Button>
+      </Box>
+    </div>
+  );
+};
+
+export default page;
